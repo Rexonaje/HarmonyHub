fda93fd35e80b0c7608075322d0d2081eea3b32f
Carlos Nicolas Brambilla <97968900+Rexonaje@users.noreply.github.com>
2024-08-27 16:42:58 -0300

    Alumnos controller y model

--- model/Alumnos.php
+++ model/Alumnos.php
@@ -1,5 +1,19 @@
 <?php
  namespace Model;
- class Alumnos{
-    
+ class Alumnos extends ActiveRecord{
+    protected static $tabla='alumnos';
+    protected static $columnasBD=['alumnos_id','nombre','asignaciones_asignaciones_id','comentarios'];
+
+    public $alumnos_id;
+    public $nombre;
+    //public $asignaciones_asignaciones_id;
+    public $comentarios;
+
+    public function __construct($args=[])
+    {
+        $this->alumnos_id=$args['alumnos_id'];
+        $this->nombre=$args['nombre'];
+        //$this->asignaciones_asignaciones_id=$args['asignaciones_asignaciones_id'];
+        $this->comentarios=$args['comentarios'];
+    }
  }
--- model/Horiarios.php
+++ model/Horiarios.php
@@ -1,5 +1,5 @@
 <?php
  namespace Model;
- class Horarios{
+ class Horarios  extends ActiveRecord{
     
  }
